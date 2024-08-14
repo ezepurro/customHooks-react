a05cb9e94ed5c21ba9675bef5c340dbc522f19c4
Ezequiel Purro <ezequiel.purro@gmail.com>
2024-08-14 20:32:58 -0300

    Primeros hooks

---
+++ useForm.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const useForm = ( initalForm = {} ) => {
+
+    const [formState, setFormState] = useState(initalForm)
+
+    const onInputChange = ({target}) => {
+        const {name, value} = target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        })
+
+    }
+
+    const onReset = () => {
+        setFormState(initalForm);
+    }
+
+    return {
+        ...formState,
+        formState,
+        onInputChange,
+        onReset,
+    }
+}
+
+export default useForm
+
+// {
+//     username: '',
+//     email: '',
+//     password: '',
+// }
